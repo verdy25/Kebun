3316d8e80431c37c24e5c964627199d0e6d39062
Verdy Bangkit <verdybangkit@gmail.com>
2019-06-23 16:15:58 +0700

    add btnRefresh transaksi

--- src/controller/TransaksiController.java
+++ src/controller/TransaksiController.java
@@ -5,6 +5,9 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
 import model.TransaksiModel;
 import view.PengusahaDftrTrx;
 import view.PetaniDftrTrx;
@@ -22,13 +25,35 @@ public class TransaksiController {
         this.v = v;
         this.m = m;
         v.setVisible(true);
-        this.v.setTabel(v.getTable(), m.tableTransaksi());
+        v.setTabel(v.getTable(), m.tableTransaksi());
+        v.btnRefresh().addActionListener(new refresh());
     }
     
     public TransaksiController(TransaksiModel m, PengusahaDftrTrx v2){
         this.v2 = v2;
         this.m = m;
         v2.setVisible(true);
-        this.v2.setTabel(v2.getTable(), m.tableTransaksi());
+        v2.setTabel(v2.getTable(), m.tableTransaksi());
+        v2.btnRefresh().addActionListener(new refresh());
+    }
+    
+    private class refresh implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DefaultTableModel model = (DefaultTableModel) v.getTable().getModel();
+            model.setRowCount(0);
+            m.updateTableTransaksi(model);
+        }
+    }
+    
+    private class refresh2 implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DefaultTableModel model = (DefaultTableModel) v2.getTable().getModel();
+            model.setRowCount(0);
+            m.updateTableTransaksi(model);
+        }
     }
 }
